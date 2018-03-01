e1724b6e9e8f2934c2839b309656ca972090d587
McAlexVrn <mcalexvrn@yandex.ru>
2018-03-01 10:47:17 +0300

    Update engine.rb

--- lib/active_admin/editor/engine.rb
+++ lib/active_admin/editor/engine.rb
@@ -7,21 +7,21 @@ module ActiveAdmin
         def editor; ActiveAdmin::Editor.configuration end
       end
 
-      initializer 'active_admin.editor', :group => :all do |app|
-        app.config.assets.precompile += [
-          'active_admin/editor.js',
-          'active_admin/editor.css'
-        ] + ActiveAdmin::Editor.configuration.stylesheets
+#       initializer 'active_admin.editor', :group => :all do |app|
+#         app.config.assets.precompile += [
+#           'active_admin/editor.js',
+#           'active_admin/editor.css'
+#         ] + ActiveAdmin::Editor.configuration.stylesheets
 
-        ActiveAdmin.application.tap do |config|
-          config.register_javascript 'active_admin/editor.js'
-          config.register_stylesheet 'active_admin/editor.css'
+#         ActiveAdmin.application.tap do |config|
+#           config.register_javascript 'active_admin/editor.js'
+#           config.register_stylesheet 'active_admin/editor.css'
 
-          ActiveAdmin::Editor.configuration.stylesheets.each do |stylesheet|
-            config.register_stylesheet stylesheet
-          end
-        end
-      end
+#           ActiveAdmin::Editor.configuration.stylesheets.each do |stylesheet|
+#             config.register_stylesheet stylesheet
+#           end
+#         end
+#       end
     end
   end
 end
